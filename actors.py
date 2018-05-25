0134d5c6a7102098326987b8cf8945042840ad4a
martin-martin <breuss.martin@gmail.com>
2018-05-25 17:17:02 +0700

    :seedling: commit

---
+++ actors.py
@@ -0,0 +1,20 @@
+
+class Wizard(object):
+    """docstring for Wizard"""
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+class Creature(object):
+    """docstring for Creature"""
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+        class Creature(object):
+
+class SmallAnimal(Creature):
+    """docstring for SmallAnimal"""
+    def __init__(self, arg):
+        super(SmallAnimal, self).__init__()
+        self.arg = arg
+
---
+++ main.py
@@ -0,0 +1,12 @@
+
+def play_game():
+
+    creatures = [
+        Creature('Grasshopper', 1)
+        Creature('Frog', 5)
+        Creature('Boar', 10)
+        Creature('Wolf', 20)
+        Creature('Bear', 30)
+        Creature('Dragon', 50)
+        Creature('Wizard', 500)
+    ]
